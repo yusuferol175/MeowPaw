ebba33962ea839eb95798d86eaedd9c999498e5c
yusuferol175 <78260853+yusuferol175@users.noreply.github.com>
2023-05-25 12:40:43 +0300

    Update kedi_haller.cs

--- Assets/codes/kedi_haller.cs
+++ Assets/codes/kedi_haller.cs
@@ -19,11 +19,6 @@ public class kedi_haller : MonoBehaviour
            
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
